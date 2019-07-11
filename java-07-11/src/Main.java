e3b6a87047e7e861dc2871c161fffc821fd30253
Martin Dezsi <dezsimartin@gmail.com>
2019-07-11 09:23:35 +0200

    modified gitignore

--- java-07-11/src/Main.java
+++ java-07-11/src/Main.java
@@ -4,10 +4,12 @@ public class Main {
         int a = 10;
         int b = 3;
         double res = a / b;
-        System.out.println(res);
+        System.out.println(res);            //két egész számnál az eredmény egész
+        System.out.println(divide(a,b));    //double-ként kezeli az egész számot, amit kapott paraméterben
     }
 
     public static double divide(int a, int b){
-
+        double res = (double) a / b;
+        return res;
     }
 }
