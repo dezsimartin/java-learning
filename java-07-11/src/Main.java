6fde3e2ef1ddee2c6bd8ac25c93ad993285d7e9d
Martin Dezsi <dezsimartin@gmail.com>
2019-07-11 09:18:43 +0200

    adding java 07-11

---
+++ java-07-11/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        double res = a / b;
+        System.out.println(res);
+    }
+
+    public static double divide(int a, int b){
+
+    }
+}
