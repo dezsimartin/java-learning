fe71e22236cb5aee285896c0d91c6fb6a6e435cc
Martin Dezsi <dezsimartin@gmail.com>
2019-08-02 11:18:36 +0200

    update

---
+++ EnumStreamLambda/src/com/company/Kiscica.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Kiscica {
+
+    private String name;
+    private int age;
+
+    public Kiscica(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Kiscica kiscica = (Kiscica) o;
+        return getAge() == kiscica.getAge() &&
+                getName().equals(kiscica.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- EnumStreamLambda/src/com/company/Main.java
+++ EnumStreamLambda/src/com/company/Main.java
@@ -1,13 +1,82 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
-    enum Planet {   //konstans értékeket tárolnak
-        MARS, EARTH, JUPITER
+    enum Planet {                                                                //konstans értékeket tárolnak
+        MARS(3402.5, 6.41E+23),                                     //értékek, amelyek meghivják az enum konstruktorát.
+        EARTH(6378.137, 5.97E+24),
+        JUPITER(71492, 1.9E+27);
+
+        private final double radius;
+        private final double mass;
+
+        Planet(double radius, double mass){
+            this.radius = radius;
+            this.mass = mass;
+        }
+
+        public double getRadius(){ return radius; }
+        public double getMass() { return mass; }
     }
 
     public static void main(String[] args) {
 
         Planet p = Planet.EARTH;
+        System.out.println(p.getMass());
+        if (p == Planet.EARTH){
+            System.out.println("zacsi");
+        }
+
+
+        List<String> nameList = Arrays.asList("Feri", "Robi", "Máté1", "fMáté2");
+        nameList.stream().sorted().limit(2).forEach(System.out::println);
+        List<String> newNameList =  nameList.stream().sorted().limit(2).collect(Collectors.toList());
+        System.out.println(newNameList);
+        newNameList.stream().map(s -> s.toUpperCase()).forEach(System.out::println);
+        nameList.stream().map(s -> s.length()).sorted(Comparator.reverseOrder()).limit(1).forEach(System.out::println);
+        nameList.stream().filter(s -> s.charAt(0) == 'F' || s.charAt(0) == 'f').forEach(item -> {System.out.println(item + " ");});   //lambda expression item -> {System.out.println(item + " ");}
+
+        newCSV("kiscica.csv");
+
     }
+
+    public static void newCSV(String path){
+        try{
+            Files.lines(Paths.get(path)).forEach(item -> {
+                System.out.println(item);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readOldCSV(String path){
+        try{
+            // Files.lines(Path.get(path))
+            BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+            String s = br.readLine();
+            while(s != null) {
+                System.out.println(s);
+                s = br.readLine();
+            }
+
+        }  catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
