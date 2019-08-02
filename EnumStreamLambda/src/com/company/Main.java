db0aecd3bdd61cf173e1d49328a0bbd105fb511e
Martin Dezsi <dezsimartin@gmail.com>
2019-08-02 09:18:29 +0200

    EnumStreamLambda

---
+++ EnumStreamLambda/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    enum Planet {   //konstans értékeket tárolnak
+        MARS, EARTH, JUPITER
+    }
+
+    public static void main(String[] args) {
+
+        Planet p = Planet.EARTH;
+    }
+}
